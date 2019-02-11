50e4bd9ae22e6ca4821468f668a373a43110dd0b
Sneha Elsa Alex <sneha.alex@attinadsoftware.com>
2019-02-11 18:21:45 +0530

    data-ui added

---
+++ Data.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Booking-Data</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</head>
+<body>
+<table class="table table-bordered table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Monday</th>
+      <th scope="col">Tuesday</th>
+      <th scope="col">Wednesday</th>
+	  <th scope="col">Thrusday</th>
+	  <th scope="col">Friday</th>
+	  <th scope="col">Saturday</th>
+	  <th scope="col">Sunday</th>
+   	</tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td>1</td>
+      <td>2</td>
+	  <td>3</td>
+      <td>4</td>
+      <td>5</td>
+      <td>6</td>
+      <td>7</td>
+	</tr>
+	
+	<tr>
+      <th scope="row"></th>
+      <td>8</td>
+      <td>9</td>
+	  <td>10</td>
+      <td>11</td>
+      <td>12</td>
+      <td>13</td>
+      <td>14</td>
+	</tr>
+	
+	<tr>
+      <th scope="row"></th>
+      <td>15</td>
+      <td>16</td>
+	  <td>17</td>
+      <td>18</td>
+      <td>19</td>
+      <td>20</td>
+      <td>21</td>
+	</tr>
+			
+	<tr>
+      <th scope="row"></th>
+      <td>22</td>
+      <td>23</td>
+	  <td>24</td>
+      <td>25</td>
+      <td>26</td>
+      <td>27</td>
+      <td>28</td>
+	</tr>
+	
+	<tr>
+      <th scope="row"></th>
+      <td>29</td>
+      <td>30</td>
+	  <td>31</td>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+      <td>4</td>
+	</tr>	      
+  </tbody>
+</table>
+</body>
+</html>
